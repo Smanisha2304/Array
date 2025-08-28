f5b1b945b1f273826097603ec4b805234557fea7
Manisha Sharma <alwsmilemansu23@gmail.com>
2025-08-29 00:08:22 +0530

    PlusOne.java

--- PlusOne.java
+++ PlusOne.java
@@ -1,6 +1,6 @@
 class Solution {
     public int[] plusOne(int[] digits) {
-       int n = digits.length;
+       int n = digits.length; 
        for(int i =n-1;i>=0;i--)
        {
         if(digits[i]<9)
@@ -14,4 +14,5 @@ class Solution {
        a[0]=1;
        return a;
     }
+
 }
