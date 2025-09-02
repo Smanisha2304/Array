055bccbd20128860f50763bc1a02166d82bd229a
Manisha Sharma <alwsmilemansu23@gmail.com>
2025-09-02 23:47:19 +0530

    Add files via upload

---
+++ Third.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int thirdMax(int[] nums) {
+        // // Arrays.sort(nums);
+         int max=0;
+        TreeSet<Integer> set = new TreeSet<>(Collections.reverseOrder());
+        for(int num:nums)
+        {
+            set.add(num);
+        }
+        if(set.size()<3)
+        {
+            return set.first();
+        }
+        set.pollFirst();
+        set.pollFirst();
+        return set.first();
+
+
+    }
+}
